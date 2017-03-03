df029e3569d0facca0a932c398774e1028c8a2ae
JLeppa <johannes.leppa@gmail.com>
2017-03-02 20:52:20 -0800

    Set up tests

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,9 +1,9 @@
-def test_1:
+def test_1():
     assert 1+1 == 2
 
-def test_2:
+def test_2():
     assert 2*4 == 8
 
-def test_3:
+def test_3():
     from src.project import multiplier
     assert 2*3 == multiplier(2,3)
